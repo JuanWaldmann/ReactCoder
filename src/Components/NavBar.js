49fcfa34a2e2a21cebaeec84be4112b2e6875206
Juan Ignacio <juan.waldmann@gmail.com>
2022-09-06 23:48:33 -0300

    crea NavBar inicial

---
+++ src/Components/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const NavBar = () => {
+    return (
+        <>
+        <img src="https://dummyimage.com/150x150/000/fff" alt="logo" />
+            <h1>Bienvenidos a Cobayashi Hardware</h1>
+            <nav>
+                <ul>
+                    <li><a href="">Placas de video</a></li>
+                    <li><a href="">Motherboards</a></li>
+                    <li><a href="">CPU</a></li>
+                    <li><a href="">Almacenamiento</a></li>
+                    <li><a href="">Refrigeracion</a></li>
+                    <li><a href="">Gabinetes</a></li>
+                </ul>
+            </nav>
+                <img src="https://dummyimage.com/64x64/000/fff" alt="carrito" />
+        </>
+            
+    )
+}
+
+export default NavBar;
